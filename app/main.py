02dd7df05bebdb431a761fc23fb7eb86e7469397
gitkvark <github@kvark.fr>
2024-03-15 22:19:11 +0100

    Chainguard course

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from app.db import database, User
+
+
+app = FastAPI(title="FastAPI, Docker, and Traefik")
+
+
+@app.get("/")
+async def read_root():
+    return await User.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
